ef339e3d6ff5ed96fc5cf1ce9338879d82aba95e
Varun Ramakrishnan <oceans.varun@gmail.com>
2019-08-19 18:49:27 -0400

    Sine Wave Simulation
    Drawing a Sine Wave manually without library functions using a cube with particles

---
+++ SineWave/SineWave_Simulation/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	public GameObject cube;
+	private float xAmp = 10f;
+	private float yAmp = 5f;
+	private float xSpeed = 1f;
+	private float ySpeed = 5f;
+	private float index = 0;
+	private float xPos;
+	private float yPos;
+
+	private void FixedUpdate()
+	{
+		SineMovement();
+	}
+
+	//Function to move the object along a sine wave
+	public void SineMovement()
+	{
+		index += Time.fixedDeltaTime;
+
+		xPos = xAmp * Mathf.Cos(xSpeed * index);
+		yPos = yAmp * Mathf.Sin(ySpeed * index);
+
+		cube.transform.localPosition = new Vector3(xPos, yPos, 0);
+	}
+}
