f7698e6698acc3e5ec32d5870ea8b41cd8393425
Varun Ramakrishnan <oceans.varun@gmail.com>
2019-08-19 18:58:05 -0400

    Shortes Path AI
    A program that uses A* pathfinding algorithm to find the shortes path from one game object to another.

---
+++ ShortestDistance/Assets/Scripts/FindPathScript.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindPathScript : MonoBehaviour 
+{
+
+    public Transform seeker, target;
+    GridScript grid;
+
+    void Awake()
+    {
+        grid = GetComponent<GridScript>();
+    }
+
+    void Update()
+    {
+        FindPath(seeker.position, target.position);
+    }
+
+    void FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        NodeScript startNode = grid.NodeFromWorldPoint(startPos);
+        NodeScript targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<NodeScript> openSet = new List<NodeScript>();
+        HashSet<NodeScript> closedSet = new HashSet<NodeScript>();
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            NodeScript node = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].fCost < node.fCost || openSet[i].fCost == node.fCost)
+                {
+                    if (openSet[i].hCost < node.hCost)
+                        node = openSet[i];
+                }
+            }
+
+            openSet.Remove(node);
+            closedSet.Add(node);
+
+            if (node == targetNode)
+            {
+                RetracePath(startNode, targetNode);
+                return;
+            }
+
+            foreach (NodeScript neighbour in grid.GetNeighbours(node))
+            {
+                if (!neighbour.walkable || closedSet.Contains(neighbour))
+                {
+                    continue;
+                }
+
+                int newCostToNeighbour = node.gCost + GetDistance(node, neighbour);
+                if (newCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                {
+                    neighbour.gCost = newCostToNeighbour;
+                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    neighbour.parent = node;
+
+                    if (!openSet.Contains(neighbour))
+                        openSet.Add(neighbour);
+                }
+            }
+        }
+    }
+
+    void RetracePath(NodeScript startNode, NodeScript endNode)
+    {
+        List<NodeScript> path = new List<NodeScript>();
+        NodeScript currentNode = endNode;
+
+        while (currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        path.Reverse();
+
+        grid.path = path;
+
+    }
+
+    int GetDistance(NodeScript nodeA, NodeScript nodeB)
+    {
+        int dstX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int dstY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if (dstX > dstY)
+            return 14 * dstY + 10 * (dstX - dstY);
+        return 14 * dstX + 10 * (dstY - dstX);
+    }
+}
