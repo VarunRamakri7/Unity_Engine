6a32c01596be45a2a26b8bac8a4624234cc3b693
Varun Ramakrishnan <oceans.varun@gmail.com>
2019-08-19 19:03:03 -0400

    UI Module Example
    A possible UI Module for a game.

---
+++ UIModule/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour 
+{
+    public Button play;
+    public Button settings;
+    public Button exit;
+    public Button yes;
+    public Button no;
+    public GameObject exitPopup;
+
+    private void Start()
+    {
+        exitPopup.SetActive(false);
+    }
+
+    public void PlayClick ()
+	{
+		play.image.rectTransform.sizeDelta *= 2;
+
+		SceneManager.LoadScene ("PlayScene");
+	}
+
+    public void SettingsClick ()
+	{
+		settings.image.rectTransform.sizeDelta *= 2;
+		SceneManager.LoadScene ("SettingsScene");
+	}
+
+    public void ExitClick ()
+	{
+        exit.image.rectTransform.sizeDelta *= 2;
+        exitPopup.SetActive(true);
+	}
+
+    public void YesClick()
+    {
+        Application.Quit();
+    }
+
+    public void NoClick()
+    {
+        //exitPopup.SetActive(false);
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ UIModule/Assets/Scripts/PlayMenuScript.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayMenuScript : MonoBehaviour
+{
+    public Button arcadeBack;
+    public Button quickBack;
+    public Button locationBack;
+    public GameObject quickStartPopup;
+    public GameObject locationPopup;
+    public GameObject arcadePopup;
+
+    private void Start()
+    {
+        quickStartPopup.SetActive(false);
+        locationPopup.SetActive(false);
+        arcadePopup.SetActive(false);
+    }
+
+    public void ArcadeClick()
+    {
+        arcadePopup.SetActive(true);
+    }
+
+    public void LocationClick()
+    {
+        locationPopup.SetActive(true);
+    }
+
+    public void QuickClick()
+    {
+        quickStartPopup.SetActive(true);
+    }
+
+    public void ArcadeBack()
+    {
+        arcadePopup.SetActive(false);
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void QuickBack()
+    {
+        quickStartPopup.SetActive(false);
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void LocationBack()
+    {
+        locationPopup.SetActive(false);
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void BackMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
